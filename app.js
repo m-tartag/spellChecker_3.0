443979ed1c3969ae329b39798a1ffde8c3333cb4
m-tartag <mattbrs51@gmail.com>
2019-05-05 23:57:10 -0400

    working on app.js - front end integration

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
-
 const mongoose = require('mongoose');
 
-
+// Initialize app
 
 const app = express();
 
 
-// Connect to MongoDB (Mongoose)
+
+// [Connect to MongoDB w/ Mongoose]
 
 mongoose.connect('mongodb://localhost/wordDatabase', {
   useNewUrlParser: true
@@ -23,28 +22,50 @@ mongoose.connect('mongodb://localhost/wordDatabase', {
 
 
 
-// ======= Middleware ========
+// [*~*~*~* Middleware *~*~*~*]
 
 
-// [Body-Parser]
+
+// 1. [Body-Parser]
 
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
+// 2. [Express.static] 
+
+app.use(express.static('public'));
+
+// 3. HandleBars
+
+
+// 4. 
 
 
 
 
+// [*~*~*~* Routes *~*~*~*]
 
-
-
-// ======= Routes ========
+// [Index]
 
 app.get('/', (req, res) => {
-  const title = "Spell Checker";
-  res.render('index', {
+  const title = 'Spell Checker';
+  res.render(index.html, {
     title: title
-  })
+  });
+  
+})
+
+// [About]
+
+app.get('/about', (req, res) => {
+  res.render(about.html);
+  
+})
+
+// [Create Account]
+
+app.get('/account/new', (req, res) => {
+  res.render(newAccount.html);
 })
 
 
