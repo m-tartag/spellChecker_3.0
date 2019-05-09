6bd26d3718c82aaa443becabdd75b9a236837cda
m-tartag <mattbrs51@gmail.com>
2019-05-09 03:38:30 -0400

    cleaning up code

--- public/script.js
+++ public/script.js
@@ -4,31 +4,26 @@ $(document).ready(() => {
   $('#submitButton').click(() => {
     const input = document.getElementById('inputTextArea').value;
     const Url = 'http://localhost:4000/api'
+    const checkMark = `<i class="fas fa-trophy"></i>`
+    const wrongMark = `<i class="fas fa-skull"></i>`
 
     $.ajax({
         url: Url, 
         type: 'GET',
         data: {word: input},
         success(result) {
-          
           if (result.length > 0) {
               
-              const checkMark = `<i class="fas fa-trophy"></i>`
-
-
               $('.result').html('')
               $('#result').removeClass('resultToggle').removeClass('resultGreen').removeClass('resultRed').addClass('resultGreen')
               $('.result').append(`${input} ${checkMark}`)
-              
             
           } else {
               
-              const wrongMark = `<i class="fas fa-skull"></i>`
-
               $('.result').html('')
               $('#result').removeClass('resultToggle').removeClass('resultRed').removeClass('resultGreen').addClass('resultRed')
               $('.result').append(`${input} ${wrongMark}`)
-            
+              
           }
         },
         error(error) {
