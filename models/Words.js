28ff6b9a071d391a509b9c561552f762cd4192ac
m-tartag <mattbrs51@gmail.com>
2019-05-10 01:04:22 -0400

    cleaning up code

--- models/Words.js
+++ models/Words.js
@@ -1,6 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 
 // [Schema]
 
@@ -8,5 +7,4 @@ const WordSchema = new mongoose.Schema({
   word: String
 });
 
-
-mongoose.model('words', WordSchema);
+mongoose.model("words", WordSchema);
--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
   $('#submitButton').click(() => {
     const input = document.getElementById('inputTextArea').value;
     const Url = 'http://localhost:4000/api'
-    const defineUrl = `https://dictionaryapi.com/api/v3/references/collegiate/json/${input}?key=`
+    const defineUrl = `https://dictionaryapi.com/api/v3/references/collegiate/json/${input}?key=782db00d-932d-43aa-9e2d-053051f3251b`
     const checkMark = `<i class="fas fa-trophy"></i>`
     const wrongMark = `<i class="fas fa-skull"></i>`
 
